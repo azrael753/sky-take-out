2c91d0525188abab8fcb8b2f64b47a473704737c
rossignol <2746054243@qq.com>
2024-12-05 16:58:35 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -40,4 +41,12 @@ public interface OrderMapper {
     void updateStatus(
             Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time, Long id);
 
+    /**
+     * 根据订单状态和时间查询订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
